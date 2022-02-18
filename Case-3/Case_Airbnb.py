3389e6606d57ff9444bf80e8a65e7af37cfc255f
garacevedo <gabrielandres.angel@gmail.com>
2022-02-18 12:47:45 -0500

    Complete 2 objectives

--- Case-3/Case_Airbnb.py
+++ Case-3/Case_Airbnb.py
@@ -17,6 +17,6 @@ for i in df.columns.values:
 
 df["avg_filas"]=df[col_reviews].mean(axis=1)
 
-# Las mejores puntuaciones y los precios
-print("Las mejores puntuaciones y sus respectivos precios")
+# Objetivo 1
+
 print(df[["avg_filas","price","property_type"]].sort_values(by=[ "avg_filas","price"], ascending=False).head(20))
